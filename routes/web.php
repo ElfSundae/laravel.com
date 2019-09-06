7246fcc37750b51a222fbb843335b146a3b084e1
Elf Sundae <elf.sundae@gmail.com>
2019-09-06 12:04:26 +0800

    Add 6.0 docs

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@
  * Set the default documentation version...
  */
 if (! defined('DEFAULT_VERSION')) {
-    define('DEFAULT_VERSION', '5.8');
+    define('DEFAULT_VERSION', '6.0');
 }
 
 Route::get('/', function () {
